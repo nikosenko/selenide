23db7ea3842db8a871fc47c9efe3815893b4110f
Natalia test <n.i.kosenko@bk.ru>
2024-05-17 20:35:23 +0300

    heppy path

---
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -0,0 +1,39 @@
+package ru.netology;
+
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class CardDeliveryTest {
+    private String generateData(long addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+
+    }
+
+    @Test
+    public void successfulCompeleted() {
+        open("http://localhost:9999");
+        $("[data-test-id='city'] input").setValue("Кострома");
+        String actualDate = generateData(3, "dd.MM.yyyy");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date'] input").setValue(actualDate);
+        $("[data-test-id='name'] input").setValue("Котовский Константин");
+        $("[data-test-id='phone'] input").setValue("+79270000000");
+        $("[data-test-id='agreement']").click();
+        $(".button.button").click();
+        $(".notification__content")
+
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldBe(Condition.exactText("Встреча успешно забронирована на " + actualDate));
+
+
+    }
+}
